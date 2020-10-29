cee53ffa74da22235af9e62261f94a118e5fe6cc
Akira Okada <aokada@aokada-a01.vmware.com>
2020-10-30 03:25:09 +0900

    modified

---
+++ pipeline_create_cluster_test/deploy_logs_task.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# 1. Install following
+# kubectl
+# vsphere docker plugin
+# tkg CLI
+
+# 2. Login to Supervisor Cluster
+# kubeclt vsphere-login
+
+# 3. Create TKC
+# kubectl apply -f <filename>.yml
+
+
+
+# kubectl apply -f 
+
+
+echo "Hello Deploy Logs Task Script"
