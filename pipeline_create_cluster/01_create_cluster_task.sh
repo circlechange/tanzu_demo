6b24caf59c6e5fda2742a9557984ccd81ae6e2a2
Akira Okada <aokada@aokada-a01.vmware.com>
2020-10-31 04:09:04 +0900

    updated

--- pipeline_create_cluster/01_create_cluster_task.sh
+++ pipeline_create_cluster/01_create_cluster_task.sh
@@ -14,12 +14,24 @@ echo "Hello Create Cluster Task Script"
 echo "\n\n$ whoami"
 whoami
 
+echo "\n\n$ pwd"
+pwd
+
 echo "\n\nCheck Directory"
 echo "$ ls -al"
 ls -al
 
 echo "\n\n$ ls -al resource-tanzu"
 ls -al resource-tanzu
+
+echo "\n\n$ ls -al resource-tanzu/tanzu_clusters"
+ls -al resource-tanzu/tanzu_clusters
+
+echo "\n\n$ cd resource-tanzu/tanzu_clusters"
+cd resource-tanzu/tanzu_clusters
+
+echo "\n\n$ ls -al"
+ls -al
 
 echo "\n\n$ ls -al work-image"
 ls -al work-image
